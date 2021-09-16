4a6dd81ad62dbbb22d1f2e494cb621c8ac4aca94
luisjaviercases <lujacafe@gmail.com>
2021-09-16 12:07:33 +0200

    Created local files and import them to i18n file

--- src/i18n.js
+++ src/i18n.js
@@ -1,69 +1,11 @@
 import { createI18n } from "vue-i18n";
 
-const messages = {
-  en: {
-    goBackComponent: {
-      back: "Go back",
-    },
-    switchLanguage: {
-      select: "Select language",
-    },
-    header: {
-      title: "RSS Reader",
-    },
-    listItem: {
-      viewMore: "View more",
-    },
-    searchInput: {
-      placeholder: "Search...",
-      description: "Filter by title: ",
-    },
-    elementList: {
-      info: "Click to change: ",
-      orderTitle: "Sorted by date ",
-      ascending: "ascending",
-      descending: "descending",
-      loading: "Loading...",
-      noResultsToShow: "No results to show",
-    },
-    addFeedInput: {
-      info: "Insert a valid feed URL and press enter key: ",
-    },
-  },
-  es: {
-    goBackComponent: {
-      back: "Atrás",
-    },
-    switchLanguage: {
-      select: "Seleccionar idioma",
-    },
-    header: {
-      title: "Lector RSS",
-    },
-    listItem: {
-      viewMore: "Ver más",
-    },
-    searchInput: {
-      placeholder: "Buscar...",
-      description: "Filtrar por título: ",
-    },
-    elementList: {
-      info: "Click para cambiar: ",
-      orderTitle: "Ordenado ",
-      ascending: "ascendentemente",
-      descending: "descendentemente",
-      loading: "Cargando...",
-      noResultsToShow: "No hay resultados que mostrar",
-    },
-    addFeedInput: {
-      info: "Introduzca una URL válida de feed y presione la tecla enter: ",
-    },
-  },
-};
-
 const i18n = createI18n({
   locale: "en",
-  messages,
+  messages: {
+    en: require("./locales/en.json"),
+    es: require("./locales/es.json"),
+  },
 });
 
 export default i18n;
